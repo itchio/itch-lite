f9477cd99da6ef4c36c724512b1fbd03f1ea4013
Amos Wenger <amoswenger@gmail.com>
2019-11-06 15:00:04 +0100

    Add travis build, move index to resources folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use web_view::WebViewBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let app = WebViewBuilder::new()
+        .title("itch lite")
+        .content(web_view::Content::Html(include_str!(
+            "./resources/index.html"
+        )))
+        .size(1280, 720)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .build()
+        .unwrap();
+    app.run().unwrap();
 }
