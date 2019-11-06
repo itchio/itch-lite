6bcf3de481273f4209d11ca93de6aa7eb36f7db9
Amos Wenger <amoswenger@gmail.com>
2019-11-06 15:21:23 +0100

    Push to itch.io

--- release/ci-build.sh
+++ release/ci-build.sh
@@ -1,19 +1,25 @@
-#!/bin/sh -xe
+#!/bin/bash -xe
 
 cargo build --release
 
 TARGET="./target/release/itch-lite"
 
-if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+if [[ -z ${TRAVIS_OS_NAME} ]]; then
+    echo "\$TRAVIS_OS_NAME can't be empty!"
+    exit 1
+fi
+
+if [[ ${TRAVIS_OS_NAME} == "windows" ]]; then
 TARGET="${TARGET}.exe"
 fi # windows os
 
 ls -lhA "${TARGET}"
 
-if [[ $TRAVIS_OS_NAME != "windows" ]]; then
+if [[ ${TRAVIS_OS_NAME} != "windows" ]]; then
 strip "${TARGET}"
 fi # not windows os
 
 ls -lhA "${TARGET}"
 
-echo "TODO: butler push"
+CHANNEL="${TRAVIS_OS_NAME}"
+butler push "${TARGET}" "itch-test-account/itch-lite:${CHANNEL}"
