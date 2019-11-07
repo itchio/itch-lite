c7ac530885d7c75bbf792bbf11414794c87555d5
Amos Wenger <amoswenger@gmail.com>
2019-11-07 13:55:00 +0100

    Add xtask for cbindgen

---
+++ vendor/tether/xtask/src/main.rs
@@ -0,0 +1,53 @@
+use std::{ffi::OsStr, path::PathBuf, process::Command, time::Instant};
+
+fn main() {
+    let task = std::env::args().skip(1).next();
+    match task.as_ref().map(String::as_str) {
+        Some(task) => match task.as_ref() {
+            "bind" => {
+                bind();
+            }
+            _ => usage(),
+        },
+        _ => usage(),
+    }
+}
+
+fn bind() {
+    println!();
+
+    let start = Instant::now();
+
+    let manifest_dir: PathBuf = std::env::var("CARGO_MANIFEST_DIR")
+        .expect("should be run from cargo")
+        .into();
+    let tether_dir = manifest_dir.parent().unwrap();
+
+    let config_file = tether_dir.join("cbindgen.toml");
+    let input_path = tether_dir.join("src").join("raw.rs");
+    let output_path = tether_dir.join("native").join("tether.h");
+
+    println!("{:>12} `{}`", "Generating", output_path.to_string_lossy());
+    println!("{:>12} `{}`", "From", input_path.to_string_lossy());
+    println!("{:>12} `{}`", "With config", config_file.to_string_lossy());
+
+    let mut child = Command::new("cbindgen")
+        .args(&[
+            OsStr::new("--config"),
+            config_file.as_os_str(),
+            OsStr::new("--output"),
+            output_path.as_os_str(),
+            input_path.as_os_str(),
+        ])
+        .spawn()
+        .expect("should be able to start cbindgen");
+    child.wait().expect("cbindgen should complete successfully");
+
+    println!();
+    println!("Done generating in {:?}", start.elapsed());
+}
+
+fn usage() {
+    println!("Usage: cargo xtask bind");
+    std::process::exit(1);
+}
